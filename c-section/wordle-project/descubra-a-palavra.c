c1beecb04cce04fbd2ef20f0cb8b10e8d9e204e1
João Pedro Rodrigues Vieira <jpedrovieira34@gmail.com>
2023-05-13 22:13:03 -0300

    Update descubra-a-palavra.c

--- c-section/wordle-project/descubra-a-palavra.c
+++ c-section/wordle-project/descubra-a-palavra.c
@@ -1,3 +1,8 @@
+/* Projeto Wordle
+ * Nome: João Pedro Rodrigues Vieira | TIA: 32281730
+ * Turma: 02N | Algoritmos e Programação II
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
@@ -5,62 +10,74 @@
 #include <time.h>
 
 /*
- * Função que verifica se uma letra de dois vetores distintos é igual e se encontra na mesma posição.
+ * Função que verifica se duas palavras são iguais.
  */
-bool mesma_posicao(char palavra[], char palpite[], int indice)
+bool palavra_correta(char palavra[], char palpite[], int len)
 {
-  if (palpite[indice] == palavra[indice]) {
-      return true;
-  }
-  return false;
-}
-
-/*
- * Função que verifica se uma letra pertence a outra palavra e se está em uma posição diferente em caso afirmativo.
- */
-bool pertence(char palavra[], char palpite[], int tam, int indice)
-{
-    for (int i = 0; i < tam; i++) {
-        if (palpite[indice] == palavra[i] && indice != i) {
-            return true;
-        }
-    }
-    return false;
-}
-
-/*
- * Função que verifica e informa se o usuário acertou a palavra, além de formatar a saída.
- */
-bool resultado_jogo(char palavra[], char palpite[])
-{
-    char simbolos[6];
-    int i, igualdade = 0;
-    int len = strlen(palpite);
-  
-    printf("\n+-----------+\n");
-    printf("| %c %c %c %c %c |\n", palpite[0], palpite[1], palpite[2], palpite[3], palpite[4]);
-    printf("|");
-    for (i = 0; i < len; i++) {
+    int igualdade = 0;
+    for (int i = 0; i < len; i++) {
         if (palpite[i] == palavra[i]) {
-              igualdade++;
-        }
-        if (mesma_posicao(palavra, palpite, i)) {
-            printf(" ^");
-        }
-        else if (pertence(palavra, palpite, len, i)){
-            printf(" !");
-        }
-        else {
-            printf(" x");
+            igualdade++;
         }
     }
-    printf(" |\n");
-    printf("+-----------+\n");
-
     if (igualdade == 5) {
         return true;
     }
     return false;
+}
+
+/*
+ * Função que verifica se uma palavra pertence à base de dados do programa.
+ */
+bool palavra_consta(char palpite[], int len) {
+    FILE *arquivo;
+    char palavra[10];
+  
+    arquivo = fopen("sem_acentos.txt", "r");
+    while(!feof(arquivo)) {
+        if (NULL != fgets(palavra, 10, arquivo) && strlen(palavra) - 2 == 5 && palavra_correta(palavra, palpite, len)) {
+            return true;
+        }
+    }
+    fclose(arquivo);
+    return false;
+}
+
+/*
+ * Função que imprime na tela o resultado do palpite do jogador.
+ */
+void resultado_jogo(char palavra[], char palpite[], int len)
+{
+    char simbolos_palpite[] = {'x', 'x', 'x', 'x', 'x'};
+    char simbolos_palavra[] = {'x', 'x', 'x', 'x', 'x'};
+    int igualdade = 0;
+  
+    printf("\n+-----------+\n");
+    printf("| %c %c %c %c %c |\n", palpite[0], palpite[1], palpite[2], palpite[3], palpite[4]);
+  
+    for (int i = 0; i < len; i++) {
+        if (palpite[i] == palavra[i]) {
+            igualdade++;
+            simbolos_palpite[i] = '^';
+            simbolos_palavra[i] = '^';
+        }
+    }
+
+    for (int c = 0; c < len; c ++) {
+        for (int j = 0; j < len; j++) {
+            if ((palpite[c] == palavra[j] && j != c) && (simbolos_palavra[j] == 'x' && simbolos_palpite[c] == 'x')) {
+              simbolos_palavra[j] = '!';
+              simbolos_palpite[c] = '!';
+          }
+      }
+    }
+    
+    printf("|");
+    for (int ind = 0; ind < len; ind++) {
+        printf(" %c", simbolos_palpite[ind]);
+    }
+    printf(" |\n");
+    printf("+-----------+\n\n");
 }
 
 
@@ -84,7 +101,7 @@ int main()
       if (NULL != fgets(palavra, 20, arq) && strlen(palavra) - 2 == 5) {
           cont++;
           if (cont == n_aleatorio) {
-              //printf("Palavra sorteada: %s", palavra);
+              // printf("Palavra sorteada: %s", palavra);
               break;
           }
       }
@@ -92,31 +109,33 @@ int main()
     fclose(arq);
 
     // Início do Jogo
-    for (i = 0; i < 12; i++) {
-        printf("-");
-        if (i == 5) {
-            printf("| DESCRUBRA A PALAVRA |");
-        }
-    }
+    printf("<------| DESCRUBRA A PALAVRA |------>");
     printf("\n\n");
   
     printf("Digite o seu nome: ");
     fgets(nome, 50, stdin);
     
     for (i = 1; i <= 6; i++) {
+        inicio = time(NULL);
+      
+        // Entrada do palpite pelo usuário
         do {
-            inicio = time(NULL);
-            
             printf("Digite uma palavra de 5 letras: ");
             scanf("%s", palpite);
             len = strlen(palpite);
-        } while (len != 5);
-      
-        acerto = resultado_jogo(palavra, palpite);
-      
-        fim = time(NULL);
+          
+            if (!palavra_consta(palpite, len)) {
+                printf("\nPalavra inválida! ");
+            }
+        } while (!palavra_consta(palpite, len));
+        
+        printf("\n<------| %d° Tentativa |------>\n", i);
+        // Mostragem do resultado ao jogador
+        resultado_jogo(palavra, palpite, len);
+        acerto = palavra_correta(palavra, palpite, len);
         
         if (acerto) {
+            fim = time(NULL);
             printf("Parabéns, você acertou!\n");
             // Escrita do resultado no arquivo
             score = fopen("scores.txt", "a");
