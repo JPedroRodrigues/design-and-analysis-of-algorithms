a99975915876ab6c6a25abba303db3b5ccc52fc2
Joao Vieira <jpedrovieira34@gmail.com>
2024-03-09 00:03:25 -0300

    finalize ex06.c

--- PAAII/practice/week05/ex06.c
+++ PAAII/practice/week05/ex06.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
 int increasingMaximum(int a[], int n) {
-    int count = 0;
+    int count = 0, max = 0;
 
     for (int i = 0;  i < n - 1; i++) {
         if(a[i] <= a[i + 1]) ++count;
-        else count = 0;
+        else {
+            ++count;
+            max = max < count? count : max;
+            count = 0;
+        }
     }
+    return max;
 }
 
 
@@ -21,7 +26,9 @@ int main() {
     int a[] = {5, 10, 3, 2, 4, 7, 9, 8, 5};
     int len = sizeof(a) / sizeof(int);
 
-    printf("")
+    printf("Na sequência ");
+    print(a, len);
+    printf("o comprimento do segmento crescente máximo é %d\n", increasingMaximum(a, len));
 
     return 0;
 }
